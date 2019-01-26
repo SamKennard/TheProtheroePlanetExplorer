968772f7f1659cd2caef5eea0047692e8dde3455
SamKennard <sjk35@sussex.ac.uk>
2019-01-26 15:29:09 +0000

    First camera implementation

--- app/src/main/java/com/example/sam/theprotheroeplanetexplorer/mainmenu.java
+++ app/src/main/java/com/example/sam/theprotheroeplanetexplorer/mainmenu.java
@@ -1,13 +1,44 @@
 package com.example.sam.theprotheroeplanetexplorer;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class mainmenu extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 101;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public void sendPhoto(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (data.getExtras() != null) {
+                Bundle extra = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extra.get("data");
+                imageView.setImageBitmap(imageBitmap);
+            } else {
+                System.out.println("u fucked it kid");
+            }
+        }
+
     }
 }
